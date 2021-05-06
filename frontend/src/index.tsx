966073f9f98322eb7577b55eea6e88c5ec2196f8
joaopioner1 <joaopioner123@gmail.com>
2021-05-06 16:03:57 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css';
 import App from './App';
 
 ReactDOM.render(
@@ -8,3 +10,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
