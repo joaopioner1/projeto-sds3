15f8b0a30816c0e469d050b33a0156e6174bdee6
joaopioner1 <joaopioner123@gmail.com>
2021-05-06 16:44:19 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+
 function App() {
   return (
+    <>
+    <NavBar />
     <div>
       <h1 className="text-primary">Olá mundo!</h1>
     </div>
+    <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/joaopioner1" target="_blank" rel="noreferrer">João Vítor Pioner</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
